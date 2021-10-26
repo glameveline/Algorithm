d0dccc0f99108e18da716c828a7e517d1b225dcd
glameveline <75834142+glameveline@users.noreply.github.com>
2021-10-26 20:03:17 +0900

    Add files via upload

---
+++ false_position.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# false position
+def false_position(f,xl,xu,es):
+	xr_old=0
+	ea=100
+	xr=xu-(f(xu)*(xl-xu))/(f(xl)-f(xu))
+	step=-1
+	while abs(ea)>es:
+		xr=xu-(f(xu)*(xl-xu))/(f(xl)-f(xu))
+		if f(xr)*f(xu)>0:
+			xu=xr
+		elif f(xr)*f(xu)<0:
+			xl=xr
+		elif abs(ea)<=es:
+			active=False
+		else:
+			print("Root is {} and the error is {}".format(xr,ea))
+			break
+		ea=(xr-xr_old)/xr*100
+		xr_old=xr
+		step+=1
+		print("step\txl\txu\txr\tea")
+		print("{} {} {} {} {}\n".format(step,xl, xu,xr,ea))
+		
+		
+f=lambda x:-12-21*x+18*x**2-2.75*x**3
+false_position(f, -1, 0, 1)
