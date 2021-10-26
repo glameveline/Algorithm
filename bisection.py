9620afd979b21e909c5c80c36e55be89dece3e23
glameveline <75834142+glameveline@users.noreply.github.com>
2021-10-26 19:58:47 +0900

    Update bisection.py

--- bisection.py
+++ bisection.py
@@ -5,9 +5,9 @@
 def bisection(f,xl,xu,es):
 	xr_old=xu
 	xr=(xl+xu)/2
-	ea=1
-	i=0
-	while ea >= es or ea <= es:
+	ea=100
+	step=-1
+	while abs(ea)>es:
 		if f(xr)*f(xu)>0:
 			xu=xr
 		elif f(xr)*f(xu)<0:
@@ -15,13 +15,13 @@ def bisection(f,xl,xu,es):
 		else:
 			print("Root is {} and the error is {}".format(xr,ea))
 			break
-		ea=(xr-xr_old)/xr
 		xr_old=xr
 		xr=(xl+xu)/2
-		i+=1
+		ea=(xr-xr_old)/xr*100
+		step+=1
 		print("step\txl\txu\txr\tea")
-		print("{} {} {} {} {}\n".format(i,xl, xu,xr,ea))
+		print("{} {} {} {} {}\n".format(step,xl, xu,xr,ea))
 		
 f=lambda x:-12-21*x+18*x**2-2.75*x**3
-bisection(f, -1, 0, 0.1)
+bisection(f, -1, 0, 1)
 
